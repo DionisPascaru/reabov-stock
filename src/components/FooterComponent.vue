96fa43d4f9c9e51eedb91ffbc5926a2bfa381cb4
DionisPascaru <denis.pascaru95@gmail.com>
2022-12-25 21:35:59 +0200

    Adjust footer for mobile version

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,12 +6,14 @@
         <img :src="require('@/assets/images/copyright.svg')" />
       </div>
       <div class="social-media">
-        <div class="social-media-icon">
-          <img :src="require('@/assets/images/instagram.svg')" />
-        </div>
         <div class="social-media-text">
           <span>Follow to</span>
           <span>be updated</span>
+        </div>
+        <div class="social-media-icon">
+          <a href="https://instagram.com/reabovstock?igshid=YmMyMTA2M2Y=">
+            <img :src="require('@/assets/images/instagram.svg')" />
+          </a>
         </div>
       </div>
     </div>
